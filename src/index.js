253f97248fb634757ffe087f8a5e72196ffec14f
Muhammad Aqib Javed <aqibjaved5201@gmail.com>
2023-03-30 01:47:06 +0500

    application completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import ReactDOM from 'react-dom/client';
-import React from 'react';
-import './index.css';
-import App from './App';
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
+import ReactDOM from "react-dom";
+import React from "react";
+import "./index.css";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById('root'));
